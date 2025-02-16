f58afa1f8a111972aa72a49af293630e9f09dba0
Abdulahad Hussain <129290085+ABDULAHAD118@users.noreply.github.com>
2025-02-16 22:38:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ await connection(process.env.MONGODB_URI);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
